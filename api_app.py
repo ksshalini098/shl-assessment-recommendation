198afa88a27df16d7012ad5ef70260e65d15eaea
Shalini ks <147915999+ksshalini098@users.noreply.github.com>
2026-03-03 10:02:33 +0530

    Added screenshots

--- api_app.py
+++ api_app.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 import pandas as pd
 import numpy as np
 import faiss
+import os
 from sentence_transformers import SentenceTransformer
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="SHL Assessment Recommendation API")
@@ -82,5 +83,8 @@ def recommend_assessments(request: QueryRequest):
         "query": query,
         "recommendations": results
     }
-
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
     
